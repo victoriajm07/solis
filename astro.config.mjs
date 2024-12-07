b3f792b8f47bbebd17431974e96bf15bae148593
victoriajm07 <victoria10444@gmail.com>
2024-12-07 00:22:51 +0000

    Cambios en la vista y texto para historia e inicio más diseño

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-    integrations: [react()],
-  });
+  integrations: [react()],
+});
 
 
